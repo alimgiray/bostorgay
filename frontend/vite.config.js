955a5277d327beeb1d4d6d6ff16498818717026a
Alim Giray Aytar <alimgiray@gmail.com>
2021-12-24 20:45:59 +0300

    get html title from env vars

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
+import { loadEnv } from "vite";
+import { minifyHtml, injectHtml } from "vite-plugin-html";
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      vue(),
+      minifyHtml(),
+      injectHtml({
+        injectData: {
+          title: env.VITE_APP_NAME,
+        },
+      }),
+    ],
+  };
+};
