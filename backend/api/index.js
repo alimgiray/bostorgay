7cde7e194e2c61ad2b9a86936274f53f0095c64c
Alim Giray Aytar <alimgiray@gmail.com>
2021-12-29 18:19:50 +0300

    new services

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,6 +2,7 @@ const route = require("express").Router();
 
 route.use("/users", require("../services/user/user.controller"));
 route.use("/songs", require("../services/song/song.controller"));
+route.use("/artists", require("../services/artist/artist.controller"));
 
 // Health check
 route.get("/alive", alive);
