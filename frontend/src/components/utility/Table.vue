6ec1e1072365905834ad9ae2afeca7909edbb7f1
Alim Giray Aytar <alimgiray@gmail.com>
2021-12-30 13:16:27 +0300

    use some utility components

---
+++ frontend/src/components/utility/Table.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>Table</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  data: function () {
+    return {};
+  },
+  mounted() {},
+  computed: {},
+};
+</script>
+
+<style scoped>
+</style>
