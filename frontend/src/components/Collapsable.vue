a3a13d8a98e90274dce37b76038eb70964e2748b
Alim Giray Aytar <alimgiray@gmail.com>
2022-01-04 00:32:24 +0300

    improve collapsable player

--- frontend/src/components/Collapsable.vue
+++ frontend/src/components/Collapsable.vue
@@ -4,21 +4,39 @@
     class="fixed bottom-0 max-w-lg w-full bg-gray-300 h-12"
     ref="collapsable"
   >
-    <div class="bg-gray-400 flex justify-between">
-      <div><span v-if="!expanded">Player here</span></div>
-      <ChevronDoubleUpIcon
-        @click="transform(expanded)"
-        class="mx-1 h-6 w-6 text-blue-500"
-      />
+    <div class="flex justify-between p-2 h-12 align-middle border-b">
+      <div class="flex flex-col justify-around">
+        <span class="text-center" v-if="!expanded"
+          >Currently playing song here</span
+        >
+      </div>
+      <div class="flex flex-col justify-around">
+        <ChevronDoubleUpIcon
+          v-if="!expanded"
+          @click="transform(expanded)"
+          class="mx-1 h-6 w-6 text-blue-500"
+        />
+        <ChevronDoubleDownIcon
+          v-if="expanded"
+          @click="transform(expanded)"
+          class="mx-1 h-6 w-6 text-blue-500"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ChevronDoubleUpIcon } from "@heroicons/vue/outline";
+import {
+  ChevronDoubleUpIcon,
+  ChevronDoubleDownIcon,
+} from "@heroicons/vue/outline";
 export default {
   name: "Collapsable",
-  components: { ChevronDoubleUpIcon },
+  components: {
+    ChevronDoubleUpIcon,
+    ChevronDoubleDownIcon,
+  },
   data: function () {
     return {
       expanded: false,
@@ -29,9 +47,9 @@ export default {
       this.expanded = !this.expanded;
       if (this.expanded) {
         this.$refs.collapsable.classList.remove("h-12");
-        this.$refs.collapsable.classList.add("h-64");
+        this.$refs.collapsable.classList.add("h-4/6");
       } else {
-        this.$refs.collapsable.classList.remove("h-64");
+        this.$refs.collapsable.classList.remove("h-4/6");
         this.$refs.collapsable.classList.add("h-12");
       }
     },
