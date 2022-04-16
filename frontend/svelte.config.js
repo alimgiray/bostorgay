c67161260ce074ebbe06eb1bbd777c033bf9dd10
Alim Giray Aytar <alimgiray@gmail.com>
2022-04-16 23:59:51 +0300

    use default directory for svelte output

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ out: 'my-output-directory' })
+		adapter: adapter()
 	}
 };
 
