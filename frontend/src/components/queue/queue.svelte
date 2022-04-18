703ca53601d1673e78d34558a80cbfa9f15b7ba1
Alim Giray Aytar <alimgiray@gmail.com>
2022-04-18 18:44:31 +0300

    improve queue header

--- frontend/src/components/queue/queue.svelte
+++ frontend/src/components/queue/queue.svelte
@@ -1,14 +1,30 @@
 <script>
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { ChevronDown } from '@steeze-ui/heroicons';
+
 	import { queue, visible } from '../../stores/queue.store';
+
 	import SongListItem from '../list/song.list.item.svelte';
+
+	const toggleQueue = () => {
+		visible.update((v) => !v);
+	};
 </script>
 
 {#if $visible}
 	<div class="border-t max-h-96 bg-zinc-800">
 		<div class="border-b flex justify-between bg-zinc-900">
-			<div class="pl-1">Queue</div>
-			<div class="pr-1 text-sm flex flex-col justify-center">
-				{$queue.length} song{$queue.length > 1 ? '(s)' : ''}
+			<div class="flex ml-2 py-1">
+				<div>Queue</div>
+			</div>
+			<div class="flex mr-2">
+				<div class="flex flex-col justify-center">
+					<a href="/" on:click|preventDefault={toggleQueue}>
+						<div class="cursor-pointer">
+							<Icon src={ChevronDown} theme="solid" class="color-gray-900 w-6 h-6" />
+						</div>
+					</a>
+				</div>
 			</div>
 		</div>
 		<div class="overflow-y-auto max-h-96">
