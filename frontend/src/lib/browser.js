d8f40baaa1f8351ce548906a32684684b5181b77
Alim Giray Aytar <alimgiray@gmail.com>
2022-04-24 13:06:43 +0300

    play a fake video in the background so device don't go to sleep

--- frontend/src/lib/browser.js
+++ frontend/src/lib/browser.js
@@ -1,5 +1,4 @@
 import { browser } from '$app/env';
-import NoSleep from 'nosleep.js';
 
 export function browserSet(key, value) {
 	if (browser) {
@@ -25,7 +24,43 @@ export function browserRemove(key) {
 
 export function preventSleeping() {
 	if (browser) {
-		const noSleep = new NoSleep();
-		noSleep.enable();
+		var video = document.createElement('video');
+		video.setAttribute('loop', '');
+		video.setAttribute('style', 'position: fixed;');
+
+		var base64 = function (mimeType, base64) {
+			return 'data:' + mimeType + ';base64,' + base64;
+		};
+
+		addSourceToVideo(
+			video,
+			'webm',
+			base64(
+				'video/webm',
+				'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='
+			)
+		);
+		addSourceToVideo(
+			video,
+			'mp4',
+			base64(
+				'video/mp4',
+				'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='
+			)
+		);
+
+		document.body.appendChild(video);
+		var playFn = function () {
+			video.play();
+			document.body.removeEventListener('touchend', playFn);
+		};
+		document.body.addEventListener('touchend', () => setTimeout(() => playFn(), 3000));
 	}
+}
+
+function addSourceToVideo(element, type, dataURI) {
+	var source = document.createElement('source');
+	source.src = dataURI;
+	source.type = 'video/' + type;
+	element.appendChild(source);
 }
